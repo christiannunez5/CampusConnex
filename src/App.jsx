9bfb828120ffcf1359bf54346e55af3d9dc468c0
Christian <nunezchristian77@gmail.com>
2023-11-25 18:24:06 +0800

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='bg-black'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
