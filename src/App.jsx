613dab963dc8fa6db60eb23062584ece61ccdb00
Christian <nunezchristian77@gmail.com>
2023-11-25 18:28:52 +0800

    colors setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1 className='bg-black'>hello world</h1>
+      <h1 className='bg-primary'>hello world</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ffffff",
+        secondary: {
+          1: "#0e345a",
+          2: "#DC3134"
+        }
+      }
+    },
   },
   plugins: [],
 }
